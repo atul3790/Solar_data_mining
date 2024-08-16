7b7a44cad65c0f35a2b01d251cbdbe33318b18e6
atul3790 <atul.3.7.1990@gmail.com>
2024-08-16 18:02:42 -0400

    Update time_conv.py
    Major bug edits and commenting

--- time_conv.py
+++ time_conv.py
@@ -8,15 +8,8 @@ from datetime import datetime as dt,timedelta
 
 import numpy as np
 
-##### GPS time to UTC conversion
-def gps_utc(sT):
-	'''
-	sT: GPS time stamp
-	Returns UTC equivalent time.
-	'''
-	return dt(1980, 1, 6) + timedelta(seconds=1099019056 - (35 - 19))
 
-##### UTC time to GPS conversion
+#### Leap seconds computation
 def leap_sec(sT):
 	'''
 	sT: Time in 'YYYY-MM-DDThh:mm:ss' format
@@ -24,6 +17,7 @@ def leap_sec(sT):
 	Works only for dates> 1980 Jan 1.
 	'''
 	Conv=19
+	sT=np.datetime64(sT).astype(dt)
 	epochs=[dt(1981,7,1),dt(1982,7,1),dt(1983,7,1),dt(1985,7,1),
 	dt(1988,1,1),dt(1990,1,1),dt(1991,1,1),dt(1992,7,1),dt(1993,7,1),dt(1994,7,1),
 	dt(1996,1,1),dt(1997,7,1),dt(1999,1,1),dt(2006,1,1),dt(2009,1,1),dt(2012,7,1),
@@ -36,6 +30,7 @@ def leap_sec(sT):
 			break
 	return Conv							
 
+##### GPS to UTC time conversion	
 def gps_utc(Gps):
 	'''
 	Gps: GPS time stamp
@@ -43,14 +38,16 @@ def gps_utc(Gps):
 	Works only for dates> 1980 Jan 1.
 	'''
 	tst=dt(1980, 1, 6) + timedelta(seconds=Gps - (40 - 19))
-	Conv=leap_sec(tst)			
+	Conv=leap_sec(str(tst).split('.')[0].replace(' ','T'))			
 	return dt(1980, 1, 6) + timedelta(seconds=Gps - (Conv - 19))
-	
+
+##### UTC time to GPS conversion	
 def utc_gps(sT):
 	'''
 	sT: Time in 'YYYY-MM-DDThh:mm:ss' format
 	Provides leapseconds for any given UTC time.
 	Works only for dates> 1980 Jan 1.
-	'''
+	'''	
 	Conv=leap_sec(sT)
+	sT=np.datetime64(sT).astype(dt)
 	return (sT-dt(1980,1,6)).days*86400+(sT-dt(1980,1,6)).seconds+(Conv-19)
