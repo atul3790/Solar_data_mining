e376da619ee266fa8ab67c5a891734e5db563e3e
atul3790 <atul.3.7.1990@gmail.com>
2024-08-16 16:34:25 -0400

    Create time_conv.py

---
+++ time_conv.py
@@ -0,0 +1,56 @@
+'''
+gps utc and viceversa conversion. This module has 3 functions
+1. gps_utc : Converts a given GPS time (given as an integer) to UTC
+2. utc_gps : Converts a given UTC time string ('YYYY-MM-DDThh:mm:ss') to Gps seconds
+3. leap_sec: Estimates leap seconds for a given UTC time stamp ('YYYY-MM-DDThh:mm:ss')
+'''
+from datetime import datetime as dt,timedelta
+
+import numpy as np
+
+##### GPS time to UTC conversion
+def gps_utc(sT):
+	'''
+	sT: GPS time stamp
+	Returns UTC equivalent time.
+	'''
+	return dt(1980, 1, 6) + timedelta(seconds=1099019056 - (35 - 19))
+
+##### UTC time to GPS conversion
+def leap_sec(sT):
+	'''
+	sT: Time in 'YYYY-MM-DDThh:mm:ss' format
+	Provides leapseconds for any given UTC time.
+	Works only for dates> 1980 Jan 1.
+	'''
+	Conv=19
+	epochs=[dt(1981,7,1),dt(1982,7,1),dt(1983,7,1),dt(1985,7,1),
+	dt(1988,1,1),dt(1990,1,1),dt(1991,1,1),dt(1992,7,1),dt(1993,7,1),dt(1994,7,1),
+	dt(1996,1,1),dt(1997,7,1),dt(1999,1,1),dt(2006,1,1),dt(2009,1,1),dt(2012,7,1),
+	dt(2015,7,1),dt(2017,1,1)]
+	
+	for epc in epochs:
+		if sT>=epc:
+			Conv+=1
+		else:
+			break
+	return Conv							
+
+def gps_utc(Gps):
+	'''
+	Gps: GPS time stamp
+	Returns UTC equivalent time.
+	Works only for dates> 1980 Jan 1.
+	'''
+	tst=dt(1980, 1, 6) + timedelta(seconds=Gps - (40 - 19))
+	Conv=leap_sec(tst)			
+	return dt(1980, 1, 6) + timedelta(seconds=Gps - (Conv - 19))
+	
+def utc_gps(sT):
+	'''
+	sT: Time in 'YYYY-MM-DDThh:mm:ss' format
+	Provides leapseconds for any given UTC time.
+	Works only for dates> 1980 Jan 1.
+	'''
+	Conv=leap_sec(sT)
+	return (sT-dt(1980,1,6)).days*86400+(sT-dt(1980,1,6)).seconds+(Conv-19)
