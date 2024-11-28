61fdbc724855f8cddd630b634e7ccd86bf4b3845
atul3790 <atul.3.7.1990@gmail.com>
2024-11-28 18:14:51 -0500

    Update MWA_obscat_maker.py
    updated version

--- MWA_obscat_maker.py
+++ MWA_obscat_maker.py
@@ -1,8 +1,9 @@
 '''
-	This code helps to obtain MWA observation metadata for each year falling within user-specified start and end year.
-	Finally it will build a master catalog for the whole duraiton.
-	### NOTE that the end year is also included. So ensure that there is data in the end year as well.
- 
+	This code helps to obtain MWA observation metadata for each year falling within user specified start and end year. 
+	If previous catalog exist for an year range in the repository it will 
+	update it to cover the requested full range
+
+	
 	!!!!!! BEWARE !!!!!!!!!!
 	MWA server thinks you are a scammer if this code is run more than once within a period
 	So if it fails with an HTTP 504 Gateway error its not a bug, just run the code tomorrow.
@@ -19,7 +20,7 @@ outdir='Data/All_MWA_obs/'# This is the directory where all observation details 
 pagesize=27000# This is the maximum number of observation metadata returned by the querry at a time.
 t0=2018 # Start year as int
 tf=2024 # End year as int
-buffer_days=4 # the number of days before today, till when the code can search for events.
+buffer_days=10 # the number of days before today, till when the code can search for events.
 			# if buffer_days=4. Code will search for events till today - 4 days.
 ########################################################################
 BASEURL = 'http://ws.mwatelescope.org/metadata/' # MWA metadata server
@@ -43,15 +44,16 @@ if mode==1:
 '''	
 DT=t0
 cflag=0
+lf=1
 while DT<=tf:
 	if DT>2021:
 		cflag=1
-
+	if tf
 	t1=str(DT)+'-01-01T00:00:00'
 	t2=str(DT)+'-12-31T23:59:59'
 	if np.datetime64(t2)>np.datetime64('now'):
 		t2=str(np.datetime64('now')-np.timedelta64(buffer_days,'D'))
-		
+		lf=0
 	tag=t1.split('-')[0]+'-'+t2.split('-')[0]
 	print('Getting data within: ',tag)	
 	'''
@@ -65,16 +67,32 @@ while DT<=tf:
 	if cflag==0:
 		res=pd.read_html(BASEURL+'find/?search=search&html=1&projectid=G0002&dataquality=1&notdeleted=on&pagesize='+str(pagesize)+'&mode=HW_LFILES&mintime_utc='+ST+'&maxtime_utc='+ET+'&page=1')
 	else:
-		res=pd.read_html(BASEURL+'find/?search=search&html=1&projectid=G0002&dataquality=1&notdeleted=on&pagesize='+str(pagesize)+'&mintime_utc='+ST+'&maxtime_utc='+ET+'&page=1')
-	
-	print('Found observations between ',tag)
+		if lf==0:
+			mila=False
+			counter=1
+			while mila==False:
+				try:
+					res=pd.read_html(BASEURL+'find/?search=search&html=1&projectid=G0002&dataquality=1&notdeleted=on&pagesize='+str(pagesize)+'&mintime_utc='+ST+'&maxtime_utc='+ET+'&page=1')
+					mila=True
+				except:
+					t2=t2-np.timedelta64(int(buffer_days/2),'D')
+					ET='%3A'.join(t2.split(':')[:-1])
+					counter+=1
+				if counter==5:
+					print('Unable to find enddate for the year: ',t1)
+					break	
 	bads=np.array([cl for cl in range(len(res[0])) if 'sun' not in res[0]['Obs Name'][cl].lower()])
-	Tab=res[0].drop(bads)
-	Tab.reset_index(drop=True)
-	pickle.dump(Tab,open(outdir+'MWAobs_'+tag+'_V1.p','wb'))
-	Tab.to_csv(outdir+'MWAobs_'+tag+'_V1.csv',index=False)
+	try:
+		Tab=res[0].drop(bads)
+		Tab.reset_index(drop=True)
+		pickle.dump(Tab,open(outdir+'MWAobs_'+tag+'_V1.p','wb'))
+		Tab.to_csv(outdir+'MWAobs_'+tag+'_V1.csv',index=False)
+		print('Found observations between ',tag)
+	except:
+		print('Did not find observations between ',tag)
 	DT+=1
-
+	del res
+	
 all_Tabs=sorted(glob.glob(outdir+'MWAobs_*.p'))
 
 r1=[]
