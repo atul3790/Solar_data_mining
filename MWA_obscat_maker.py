159cce75ae87c58d4e87f254ba1c6d4627b36874
atul3790 <atul.3.7.1990@gmail.com>
2024-11-28 17:59:26 -0500

    Update MWA_obscat_maker.py

--- MWA_obscat_maker.py
+++ MWA_obscat_maker.py
@@ -1,10 +1,8 @@
 '''
-	This code helps to obtain MWA observation metadata falling in a user-specified year interval. 
-  It then tabulates the observation details with date, duration, observer, and observing mode in a CSV table and 
-  a binary pickle file.
-	If there exist any previous catalogs for a year range in the output repository, the code will 
-	update it to cover the requested full range
-	
+	This code helps to obtain MWA observation metadata for each year falling within user-specified start and end year.
+	Finally it will build a master catalog for the whole duraiton.
+	### NOTE that the end year is also included. So ensure that there is data in the end year as well.
+ 
 	!!!!!! BEWARE !!!!!!!!!!
 	MWA server thinks you are a scammer if this code is run more than once within a period
 	So if it fails with an HTTP 504 Gateway error its not a bug, just run the code tomorrow.
@@ -12,17 +10,16 @@
 '''
 
 from datetime import datetime,timedelta
-import pandas as pd,glob,pickle,os
+import pandas as pd,glob,pickle,os,numpy as np
 from time_conv import utc_gps
 from datetime import datetime as dt,timedelta
 ############################ Input Parameters ########################
 basedir='/Users/amohan/Documents/NASA/' # Working directory within which data directories exist
 outdir='Data/All_MWA_obs/'# This is the directory where all observation details and catalogues are saved.
 pagesize=27000# This is the maximum number of observation metadata returned by the querry at a time.
-t0=2017 # Start year as int
+t0=2018 # Start year as int
 tf=2024 # End year as int
-#########################################################################
-
+########################################################################
 BASEURL = 'http://ws.mwatelescope.org/metadata/' # MWA metadata server
 
 cwd=os.getcwd()
@@ -32,55 +29,47 @@ if not os.path.isdir(outdir):
 	os.mkdir(outdir)
 ## If already earlier analysis results exist in the repository the code finds the last year 
 #for which the metadata is found. Then the start year is reset to the year from which there is no metadata file available
-	
+'''
+if mode==1:
+	all_exist=sorted(glob.glob(outdir+'MWAobs_*.p'))
+	yrs=[]
+	if len(all_exist)>0:
+		for i in all_exist:
+			yrs+=[int(i.split('MWAobs_')[1].replace('.p','').split('-')[1])]
+		t0=np.max(yrs)+1
+	##########################
+'''	
 DT=t0
-flag=0
 cflag=0
 while DT<=tf:
 	if DT>2021:
 		cflag=1
+
 	t1=str(DT)+'-01-01T00:00:00'
-	if DT+3<tf:
-		t2=str(DT+3)+'-12-31T23:59:59'
-	else:
-		t2=str(tf)+'-12-31T23:59:59'
-		flag=1
+	t2=str(DT)+'-12-31T23:59:59'
 		
 	tag=t1.split('-')[0]+'-'+t2.split('-')[0]
 	print('Getting data within: ',tag)	
-	
-	#minid=utc_gps(t1) # Leapseconds give the utc time + added seconds to correct for leapseconds. subtract it from GPS start date and take number of seconds to get the gps seconds time. 20 seconds are just subtracted so that you don't miss any observation around the date needed.	
-	#maxid=utc_gps(t2)
-	
+	'''
+	minid=utc_gps(t1) # Leapseconds give the utc time + added seconds to correct for leapseconds. subtract it from GPS start date and take number of seconds to get the gps seconds time. 20 seconds are just subtracted so that you don't miss any observation around the date needed.	
+	maxid=utc_gps(t2)
+	'''
 	print ('Starting to get the MWA Obs for the Solar Project....')
 	ST='%3A'.join(t1.split(':')[:-1])
 	ET='%3A'.join(t2.split(':')[:-1])
+	#res=pd.read_html(BASEURL+'find/?search=search&html=1&projectid=G0002&pagesize='+str(pagesize)+'&mode=HW_LFILES&mintime='+str(minid)+'&maxtime='+str(maxid))
 	if cflag==0:
 		res=pd.read_html(BASEURL+'find/?search=search&html=1&projectid=G0002&dataquality=1&notdeleted=on&pagesize='+str(pagesize)+'&mode=HW_LFILES&mintime_utc='+ST+'&maxtime_utc='+ET+'&page=1')
 	else:
 		res=pd.read_html(BASEURL+'find/?search=search&html=1&projectid=G0002&dataquality=1&notdeleted=on&pagesize='+str(pagesize)+'&mintime_utc='+ST+'&maxtime_utc='+ET+'&page=1')
 	
-	
-	if clfag==0:
-		res=pd.read_html(BASEURL+'find/?search=search&html=1&projectid=G0002&pagesize='+str(pagesize)+'&mode=HW_LFILES&mintime='+str(minid)+'&maxtime='+str(maxid))
-	else:
-		res=pd.read_html(BASEURL+'find/?search=search&html=1&projectid=G0002&pagesize='+str(pagesize)+'&mintime='+str(minid)+'&maxtime='+str(maxid))
 	print('Found observations between ',tag)
-	
-	## Removing calibration observations ##########################
 	bads=np.array([cl for cl in range(len(res[0])) if 'sun' not in res[0]['Obs Name'][cl].lower()])
 	Tab=res[0].drop(bads)
 	Tab.reset_index(drop=True)
-	#################################################################
-	pickle.dump(Tab,open(outdir+'MWAobs_'+tag+'.p','wb'))
-	Tab.to_csv(outdir+'MWAobs_'+tag+'.csv',index=False)	
-	if DT+4<=tf:
-		DT=DT+4
-	else:
-		DT=tf	
-	if flag==1:
-		print('Exit flag reached!! All years requested are covered!!')
-		break
+	pickle.dump(Tab,open(outdir+'MWAobs_'+tag+'_V1.p','wb'))
+	Tab.to_csv(outdir+'MWAobs_'+tag+'_V1.csv',index=False)
+	DT+=1
 
 all_Tabs=sorted(glob.glob(outdir+'MWAobs_*.p'))
 
@@ -96,4 +85,3 @@ for i in np.arange(len(all_Tabs)-1)+1:
 Tab.to_csv(outdir+'MWAobs_master_'+tag+'.csv',index=False)
 
 os.chdir(cwd)
-		
